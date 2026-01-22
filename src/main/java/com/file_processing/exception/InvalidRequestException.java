e04ae63c869c34709a8906fb14637065145369b4
Harshala Athani <47534714+harshala17@users.noreply.github.com>
2026-01-22 12:17:51 +0530

    Delete src directory

--- src/main/java/com/file_processing/exception/InvalidRequestException.java
+++
@@ -1,8 +0,0 @@
-package com.file_processing.exception;
-
-public class InvalidRequestException extends BaseException {
-
-    public InvalidRequestException(AppError error, String message) {
-        super(error, message);
-    }
-}

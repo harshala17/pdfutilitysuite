807bcda121427ee22817f936be515a657b203fce
DarshanKundu <62338963+DarshanKundu@users.noreply.github.com>
2026-02-09 00:46:13 +0530

    Delete backend/src directory

--- backend/src/main/java/com/file_processing/exception/InvalidFileFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.file_processing.exception;
-
-public class InvalidFileFormatException extends BaseException {
-    public InvalidFileFormatException(AppError error, String message) {
-        super(error, message);
-    }
-}
